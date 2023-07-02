75d36a12f47c4db6436a016d8c42e7459dcdd341
Daniel Saavedra <mantrax3.14@gmail.com>
2023-07-01 22:29:07 -0600

    lambda function

---
+++ lambda_function.py
@@ -0,0 +1,24 @@
+import json
+
+from model.product import Product
+
+
+def lambda_handler(event, context):
+    products = []
+    for req_product in event:
+        product = Product(req_product['product_id'], req_product['url'], req_product['name'], req_product['price'],
+                          req_product['scrap_field'], req_product['created_at'], req_product['updated_at'])
+        products.append(product)
+
+    products_res = []
+
+    for product in products:
+        products_res.append(
+            {"product_id": product.product_id, "name": product.name, "price": product.price, "url": product.url,
+             "scrap_field": product.scrap_field, "created_at": product.created_at, "updated_at": product.updated_at}
+        )
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps(products_res)
+    }
