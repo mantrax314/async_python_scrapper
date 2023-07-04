d732c5622f6374c20d937c04b558e2ec7bc95483
Daniel Saavedra <mantrax3.14@gmail.com>
2023-07-03 21:25:41 -0600

    product return_as_map

--- lambda_function.py
+++ lambda_function.py
@@ -11,7 +11,6 @@ def lambda_handler(event, context):
         product = Product(req_product['product_id'], req_product['url'], req_product['name'], req_product['price'],
                           req_product['scrap_field'], req_product['created_at'], req_product['updated_at'])
         products.append(product)
-
     loop = asyncio.get_event_loop()
 
     result = loop.run_until_complete(main.main(products))
@@ -19,10 +18,9 @@ def lambda_handler(event, context):
     products_res = []
 
     for product in result:
-        products_res.append(
-            {"product_id": product.product_id, "name": product.name, "price": product.price, "url": product.url,
-             "scrap_field": product.scrap_field, "created_at": product.created_at, "updated_at": product.updated_at}
-        )
+        print(product.return_as_map())
+        product_res = product.return_as_map()
+        products_res.append(product_res)
 
     return {
         'statusCode': 200,
--- model/product.py
+++ model/product.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+
+
 class Product:
 
     def __init__(self, product_id, url, name, price, scrap_field, created_at, updated_at):
@@ -8,3 +11,13 @@ class Product:
         self.scrap_field = scrap_field
         self.created_at = created_at
         self.updated_at = updated_at
+
+    def return_as_map(self):
+
+        if type(self.created_at) is datetime:
+            self.created_at = self.created_at.strftime('%Y-%m-%d %H:%M:%S')
+        if type(self.updated_at) is datetime:
+            self.updated_at = self.updated_at.strftime('%Y-%m-%d %H:%M:%S')
+
+        return {"product_id": self.product_id, "url": self.url, "name": self.name, "price": self.price,
+                "scrap_field": self.scrap_field, "created_at": self.created_at, "updated_at": self.updated_at}
